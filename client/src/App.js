12523925b939b0aa332045b525014e4e58b685fb
Claire Vita <clairej.vita@gmail.com>
2021-02-05 20:42:22 -0500

    component introduction, app.js routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,31 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Nav from './components/Nav';
+import Jumbotron from './components/Jumbotron';
+import Search from './pages/Search';
+import Saved from './pages/Saved';
 import "./App.css";
+import Unfound from "./pages/Unfound";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
+    <Router>
+      <div>
+        <Nav />
+        <Jumbotron />
+        <Switch>
+          <Route exact path={["/", "/search"]}>
+            <Search />
+          </Route>
+          <Route exact path="/saved">
+            <Saved />
+          </Route>
+          <Route>
+            <Unfound />
+          </Route>
+        </Switch>
       </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    </Router>
   );
 }
 
