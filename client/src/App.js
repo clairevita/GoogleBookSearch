cc2b164de567b391a7f6dcf6cff64d67947d0cee
Claire Vita <clairej.vita@gmail.com>
2021-02-05 20:50:15 -0500

    functioning routing

--- client/src/App.js
+++ client/src/App.js
@@ -5,23 +5,19 @@ import Jumbotron from './components/Jumbotron';
 import Search from './pages/Search';
 import Saved from './pages/Saved';
 import "./App.css";
-import Unfound from "./pages/Unfound";
 
 function App() {
   return (
     <Router>
       <div>
         <Nav />
-        <Jumbotron />
+        {/* <Jumbotron /> */}
         <Switch>
           <Route exact path={["/", "/search"]}>
             <Search />
           </Route>
           <Route exact path="/saved">
             <Saved />
-          </Route>
-          <Route>
-            <Unfound />
           </Route>
         </Switch>
       </div>
